147efc21ce5d78540423f7ff14fb8f184a4b540f
adinapop <adinampop@yahoo.com>
2021-09-13 15:48:32 +0200

    Generated 50 fake posts and printed it on page

--- database/seeds/PostsTable.php
+++ database/seeds/PostsTable.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Post;
+use Faker\Generator as Faker;
 
 class PostsTable extends Seeder
 {
@@ -11,6 +13,14 @@ class PostsTable extends Seeder
      */
     public function run(Faker $faker) 
     {
-        //
+        for($x = 0; $x < 50; $x++) {
+            $postObj = new Post();
+            $postObj->username = $faker->word(5);
+            $postObj->caption = $faker->paragraph(5);
+                                    // imageUrl(width, height, category-text on the image, gray)
+            $postObj->image = $faker->imageUrl(250, 250, 'post', true);
+            $postObj->position = $faker->word(2);
+            $postObj->save();
+        }
     }
 }
