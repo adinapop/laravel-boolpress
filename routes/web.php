12b67f290d0435303b0d758c665ee14ee4acf1ac
adinapop <adinampop@yahoo.com>
2021-09-17 13:55:37 +0200

    Deleted a comment

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ use App\Post;
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('homepage');
-// Route::get('/home', 'HomeController@index');
 
 // metodo resource di Laravel che genera tutte le rotte necessari per le crud 
 Route::resource('posts', 'PostController');
